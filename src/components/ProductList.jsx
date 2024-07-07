67c9b941307273286971e2ce8abfacfc77087e39
darrk-heart <prospaul999@gmail.com>
2024-07-07 16:56:19 +0000

    working

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CiMenuBurger, CiSearch } from "react-icons/ci";
 import { FaRegUser, FaFacebook, FaInstagram } from "react-icons/fa";
 import { GiShoppingCart } from "react-icons/gi";
 import { RiTwitterXFill } from "react-icons/ri";
+
+import logo from "../assets/F.png";
+import coco from "../assets/products/coco.png";
+import channel from "../assets/products/chanel.png";
+import amber from "../assets/products/amber.png";
+import cocomadem from "../assets/products/coco madem.png";
+import creed from "../assets/products/creed.png";
+import dng from "../assets/products/dng.png";
+import si from "../assets/products/si.png";
+import strong from "../assets/products/stronger.png";
+import versace from "../assets/products/versace.png";
+import zara from "../assets/products/zara.png";
+import zarawonder from "../assets/products/zarawonder.png";
+import red from "../assets/products/red.png";
+import roberto from "../assets/products/roberto.png";
+import ice from "../assets/products/ice cool.png";
+import givenchy from "../assets/products/givenchy.png";
+import chloe from "../assets/products/chloe.png";
 
 import "./styles.css";
 
@@ -14,13 +32,17 @@ const Header = ({ goToCheckout }) => (
         <CiMenuBurger className="menu-icon" />
         <div>
           <input className="input-field" type="text" />
-          <CiSearch />
+          <CiSearch className="icons" />
         </div>
       </div>
-      <div>logo</div>
+      <img className="logo" src={logo} alt="logo" />
       <div className="product-header-last">
-        <FaRegUser />
-        <GiShoppingCart style={{ cursor: "pointer" }} onClick={goToCheckout} />
+        <FaRegUser className="icons" />
+        <GiShoppingCart
+          className="icons"
+          style={{ cursor: "pointer" }}
+          onClick={goToCheckout}
+        />
       </div>
     </div>
     <div className="tablet-view-menu">
@@ -35,7 +57,7 @@ const Header = ({ goToCheckout }) => (
 
 const Footer = () => (
   <div className="product-footer">
-    <div>logo</div>
+    <img className="logo" src={logo} alt="logo" />
     <div>
       <span>Follow us</span>
       <FaFacebook size={20} />
@@ -45,21 +67,41 @@ const Footer = () => (
   </div>
 );
 
-const ProductItem = ({ name, price }) => (
-  <div className="item">
-    <div className="pic-background"></div>
-    <span className="product-name">{name}</span>
-    <span className="product-price">{price}</span>
-    <div className="item-operations">
-      <div>
-        <span>-</span>
-        <span>1</span>
-        <span>+</span>
+const ProductItem = ({ name, price, pic }) => {
+  const [quantity, setQuantity] = useState(1);
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  return (
+    <div className="item">
+      <div className="pic-background">
+        <img src={pic} alt="view" />
       </div>
-      <span className="addToCart">Add to cart</span>
+      <span className="product-name">{name}</span>
+      <span className="product-price">{price}</span>
+      <div className="item-operations">
+        <div>
+          <span onClick={decreaseQuantity} style={{ cursor: "pointer" }}>
+            -
+          </span>
+          <span>{quantity}</span>
+          <span onClick={increaseQuantity} style={{ cursor: "pointer" }}>
+            +
+          </span>
+        </div>
+        <span className="addToCart" style={{ cursor: "pointer" }}>
+          Add to cart
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const ProductList = () => {
   const navigate = useNavigate();
@@ -68,22 +110,22 @@ const ProductList = () => {
   };
 
   const products = [
-    { name: "Coco Noir Chanel", price: "#65,000" },
-    { name: "Channel Eau De Parfum", price: "#45,000" },
-    { name: "Chloe", price: "#37,500" },
-    { name: "Creed", price: "#85,000" },
-    { name: "Zara-Seoul 532-8", price: "#75,000" },
-    { name: "Versace Eros", price: "#105,000" },
-    { name: "Stronger with you", price: "#42,700" },
-    { name: "Roberto Cavalll", price: "#98,000" },
-    { name: "Si", price: "#40,000" },
-    { name: "Amber & Orchid", price: "#80,000" },
-    { name: "Zara wonder rose", price: "#32,000" },
-    { name: "Red diamond", price: "#68,000" },
-    { name: "Givenchy-Eau de moiselle", price: "#120,000" },
-    { name: "D&G", price: "#75,000" },
-    { name: "Coco mademoiselle", price: "#95,000" },
-    { name: "Ice cool vigor-Eau de parfum", price: "#70,000" },
+    { name: "Coco Noir Chanel", price: "#65,000", pic: coco },
+    { name: "Channel Eau De Parfum", price: "#45,000", pic: channel },
+    { name: "Chloe", price: "#37,500", pic: chloe },
+    { name: "Creed", price: "#85,000", pic: creed },
+    { name: "Zara-Seoul 532-8", price: "#75,000", pic: zara },
+    { name: "Versace Eros", price: "#105,000", pic: versace },
+    { name: "Stronger with you", price: "#42,700", pic: strong },
+    { name: "Roberto Cavalll", price: "#98,000", pic: roberto },
+    { name: "Si", price: "#40,000", pic: si },
+    { name: "Amber & Orchid", price: "#80,000", pic: amber },
+    { name: "Zara wonder rose", price: "#32,000", pic: zarawonder },
+    { name: "Red diamond", price: "#68,000", pic: red },
+    { name: "Givenchy-Eau de moiselle", price: "#120,000", pic: givenchy },
+    { name: "D&G", price: "#75,000", pic: dng },
+    { name: "Coco mademoiselle", price: "#95,000", pic: coco },
+    { name: "Ice cool vigor-Eau de parfum", price: "#70,000", pic: ice },
   ];
 
   return (
@@ -91,7 +133,11 @@ const ProductList = () => {
       <Header goToCheckout={goToCheckout} />
       <div className="product-list">
         <h5>
-          Fragrances that evoke emotions, spark memories and inspire the senses
+          Fragrances that{" "}
+          <span style={{ color: "#71360C" }}>
+            evoke emotions, spark memories
+          </span>{" "}
+          and<span style={{ color: "#71360C" }}> inspire the senses</span>
         </h5>
 
         <ul className="product-heading">
@@ -103,7 +149,12 @@ const ProductList = () => {
 
         <div className="products-view">
           {products.map((product, idx) => (
-            <ProductItem key={idx} name={product.name} price={product.price} />
+            <ProductItem
+              key={idx}
+              name={product.name}
+              price={product.price}
+              pic={product.pic}
+            />
           ))}
         </div>
         <Footer />
